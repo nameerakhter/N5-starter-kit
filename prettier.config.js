695326d4d270f31718c14e8612656608e36e25a3
nameerakhter <akhtarnameer@gmail.com>
2025-10-31 16:29:23 +0530

    fix(prettier.config.js): add tailwind plugin

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,10 @@
  * @type {import('prettier').Config}
  */
 const config = {
-	semi: false,
-	singleQuote: true,
-	trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
 }
 
 export default config
-
